7cd705d48f77bcb9abde466f9c01d45486900448
fallf <87919117+fallf@users.noreply.github.com>
2022-01-18 22:43:03 -0500

    adding information

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network', {
     // useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
